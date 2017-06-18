b2cbf9cf1863eae6a8b6b0710428bc425ed009c1
VR006\User1 <takeuchihitoshi@team-lab.com>
2017-06-18 20:45:11 +0900

    no message

---
+++ Assets/UnityOptitrack/Scripts/OptiTrackObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class OptiTrackObject : MonoBehaviour
+{
+	public int RigidbodyIndex;
+	public Vector3 PositionOffset;
+	public Vector3 RotationOffset;
+	public Vector3 LocalPositionOffset;
+	public Vector3 LocalRotationOffset;
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+		var position = OptiTrackManager.Instance.getPosition (RigidbodyIndex);
+		var rotation = OptiTrackManager.Instance.getOrientation (RigidbodyIndex);
+
+		if (!OptiTrackManager.Instance.getTracked (RigidbodyIndex)) {
+			return;
+		}
+
+		rotation *= Quaternion.Euler (RotationOffset);
+		this.transform.rotation = rotation;
+
+		position += PositionOffset;
+		this.transform.position = position;
+
+		this.transform.Translate (LocalPositionOffset);
+		this.transform.Rotate (LocalRotationOffset);
+	}
+}
